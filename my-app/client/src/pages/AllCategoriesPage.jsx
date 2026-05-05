3ebee8054feccc271545a91444728fb51a16f869
saikumar9491 <152430878+saikumar9491@users.noreply.github.com>
2026-05-05 13:55:34 +0530

    Simplify Vercel configuration for Zero Config

--- my-app/client/src/pages/AllCategoriesPage.jsx
+++ my-app/client/src/pages/AllCategoriesPage.jsx
@@ -82,7 +82,7 @@ const AllCategoriesPage = () => {
                   </button>
                 </div>
                 
-                <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+                <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 xl:gap-4">
                   {groupedMedicines[catName].map((item) => (
                     <MedicineCard key={item._id} {...item} />
                   ))}
